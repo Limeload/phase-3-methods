5ad9792534cb7870c3b9ec9728e421f7a7865826
Shraddha Rao <shraddharaom@gmail.com>
2023-01-09 02:38:15 -0600

    commit tree 44c98f4bdfe2db09b3a34f10eba27776f25f1c26

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}"
+end
+
+
+def greet_with_default(name = "Jimmy")
+    puts "Hello, #{name}!"
+end
+    
+
+def add num1, num2
+    puts num1 + num2
+end
+
+
+def halve num
+    if num != integer
+        return nil
+    puts num / 2
+end
+
+
+
+
